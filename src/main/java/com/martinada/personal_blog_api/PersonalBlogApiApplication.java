f2eec3f1d580b9f74bde902a9916e06cce097efa
martin-adaramola <169259944+martin-adaramola@users.noreply.github.com>
2024-08-21 10:58:03 +0100

    inital push

---
+++ src/main/java/com/martinada/personal_blog_api/PersonalBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.martinada.personal_blog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBlogApiApplication.class, args);
+	}
+
+}
